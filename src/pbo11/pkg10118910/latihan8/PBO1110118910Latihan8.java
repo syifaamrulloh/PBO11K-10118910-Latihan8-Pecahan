a5cf4731b8d2272799cf16876f21456ec46d0596
ADMIN <ADMIN@ADMIN-PC>
2019-09-20 23:27:06 +0700

    Latihan-Java

---
+++ src/pbo11/pkg10118910/latihan8/PBO1110118910Latihan8.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11.pkg10118910.latihan8;
+
+/**
+ *
+ * @author
+ * NAMA  : Muhamad Syifa Amrulloh 
+ * KELAS : PBO11K 
+ * NIM   : 10118910
+ *
+ * Description Program : Program berisi penggunaan Pecahan
+ */
+public class PBO1110118910Latihan8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float\t: " +f);
+        System.out.println("Double\t: " + d);
+    }
+    
+}
